40cb4c1c2f15d4d26827e3915ea47d80f0bba277
wangjiax9 <6215048wjl@163.com>
2016-07-14 22:17:55 +0800

    add more

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,23 @@
 (function(){
 	$(function(){
-		var d = ["top","right","bottom","left"];
-		for (var i = 0; i < 8; i++) {
-			var $item = $("#baseInfo li:first").clone();
-			$item.find("dl").data("animation","fadeIn-"+d[Math.floor(Math.random()*4)]);
-			$item.appendTo($("#baseInfo"));
-		}
 		$(window).scroll(function(){
 			checkAnimation();
 		});
-		
-		
+//		$("#myPortrait").on("touchmove",function(e){
+//			var touch = e.originalEvent.targetTouches[0];
+//			var w = $(window).width();
+//			var md = touch.pageX - (w/2);
+//			console.log(md);
+//			if(Math.abs(md) < (w/2)){
+//				var cp = parseInt($("#myPortrait").css("background-position-x").replace("%",""));
+//				console.log(md/100+cp);
+//				$("#myPortrait").css("background-position-x",(md/100+cp)+"%");
+//			}
+//		});
 	});
 	function checkAnimation(){
 		$("[data-animation]").each(function(){
+//			$(this).removeAttr("data-animation");
 			var h = $(window).height();
 			var st = $(document.body).scrollTop();
 			var ot = $(this).offset().top ;
@@ -22,8 +26,10 @@
 			}
 		});
 		$("[data-delay]").each(function(){
+//			$(this).removeData("delay");
 			var delay = $(this).data("delay");
 			$(this).css("animation-delay",delay);
 		});
 	}
+	
 })();
